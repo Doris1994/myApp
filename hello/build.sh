27897a43051096b186b8ec6ba4506da898ccce47
sundeqi <dsun@actiontec.com>
2016-04-14 18:28:41 +0800

    添加插件

---
+++ hello/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf ~/testbuild1-myApp
+rm -rf .meteor/local/cordova-build
+rm -rf .meteor/local/build
+rm -rf .meteor/local/bundler-cache
+rm -rf .meteor/local/plugin-cache
+#meteor -v run ios-device --mobile-server 192.168.2.25:3000
+meteor -v run ios-device --mobile-server localhost:3000
+#meteor build ~/testbuild1-myApp --server=localhost:12168
