18f063851e2eeb174245100863f2c0f331dcdbf7
sundeqi <dsun@actiontec.com>
2016-04-15 10:24:11 +0800

    修改为Meteor build的方式而不是run

--- hello/build.sh
+++ hello/build.sh
@@ -5,5 +5,5 @@ rm -rf .meteor/local/build
 rm -rf .meteor/local/bundler-cache
 rm -rf .meteor/local/plugin-cache
 #meteor -v run ios-device --mobile-server 192.168.2.25:3000
-meteor -v run ios-device --mobile-server localhost:3000
-#meteor build ~/testbuild1-myApp --server=localhost:12168
+#meteor -v run ios-device --mobile-server localhost:3000
+meteor build ~/testbuild1-myApp --server=localhost:12168
